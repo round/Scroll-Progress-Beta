00d8a11a2f4d51841001784df8dd9ad2fe930dc9
Maxim Leyzerovich <contact@round.is>
2020-04-24 00:43:59 -0700

    Basic favicon fetching

--- content.js
+++ content.js
@@ -7,6 +7,29 @@
 	  child.parentNode.removeChild(child);
 	});
 
+	var favicon = "https://www.google.com/s2/favicons?domain=" + window.location.hostname;
+	// console.log(favicon);
+
+	function toDataURL(url, callback) {
+	  var xhr = new XMLHttpRequest();
+	  xhr.onload = function() {
+    	var reader = new FileReader();
+    	reader.onloadend = function() {
+      	callback(reader.result);
+	    }
+	    reader.readAsDataURL(xhr.response);
+	  };
+	  xhr.open('GET', url);
+	  xhr.responseType = 'blob';
+	  xhr.send();
+	}
+
+	var faviconEncoded;
+	toDataURL(favicon, function(dataUrl) {
+	  faviconEncoded = dataUrl;
+	})
+
+
   var link = document.createElement('link');
   link.rel = 'icon';
 	link.type = 'image/svg';
@@ -37,7 +60,7 @@
 			// percentage = 0; //just hide the whole thing rather than default to 100
 		} else {
 			document.getElementsByTagName('head')[0].appendChild(link);
-			link.href = "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewbox=%220 0 40 40%22 width=%2240%22 height=%2240%22><circle cx=%2220%22 cy=%2220%22 r=%2210%22 fill=%22none%22 stroke=%22" + color + "%22 stroke-width=%2220%22 stroke-dasharray=%2263%22 stroke-dashoffset=%22" + percentage + "%22 shape-rendering=%22geometricPrecision%22 transform=%22rotate(-90, 20, 20)%22/></svg>";
+			link.href = "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewbox=%220 0 40 40%22 width=%2240%22 height=%2240%22><image width=%2232%22 height=%2232%22 href=%22" + faviconEncoded + "%22/><circle cx=%2220%22 cy=%2220%22 r=%2210%22 fill=%22none%22 stroke=%22" + color + "%22 stroke-width=%2220%22 stroke-dasharray=%2263%22 stroke-dashoffset=%22" + percentage + "%22 shape-rendering=%22geometricPrecision%22 transform=%22rotate(-90, 20, 20)%22/></svg>";
 		}
 	}
 
